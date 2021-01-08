8657b36e47c4f8c5c33f6a3008e7a6ad90c8c771
robond <vv.kvan@gmail.com>
2021-01-07 19:09:37 -0800

    add myself coding in process_image

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -29,7 +29,7 @@ void drive_robot(float lin_x, float ang_z)
 
 }
 
-/***
+
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
@@ -37,46 +37,69 @@ void process_image_callback(const sensor_msgs::Image img)
     int white_pixel = 255;
     float x = 0.0;
     float z = 0.0;
-    float offset_accumulated = 0;
-    int count_total = 0;
+    int ball_pixel_num = 0;
+
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
-    for(int row = 0; row < img.height; row ++)
+    for(int row = 300; row < img.height; row ++) // Because top half image is sky or wall, so from row 300
     {
-         for(int column = 0; column < img.width; column ++)
+         for(int column = 0; column < img.width / 3; column ++) //left side search
 	 {
             if (img.data[row * img.width + column] == white_pixel) {
-               
-                offset_accumulated += column - img.width / 2.0;
-                count_total++;
+                
+                ball_pixel_num++ ;
+		if (ball_pixel_num > 80){// if there is greater than 80 pixels in white
+        		drive_robot(0.0, -0.1); 
+			ROS_INFO_STREAM("Turn to Left");
+			break;					
+		} 
     	    }
 	 }
+
+         for(int column = img.width / 3; column <  (2 * img.width / 3); column ++) //middle side search
+	 {
+            if (img.data[row * img.width + column] == white_pixel) {
+                
+                ball_pixel_num++ ;
+		if (ball_pixel_num > 80){// if there is greater than 80 pixels in white
+        		drive_robot(0.2, 0.0);  // This request drives my_robot robot forward
+			ROS_INFO_STREAM("Drive forward");
+			break;					
+		} 
+    	    }
+	 }
+
+         for(int column = (2 * img.width / 3); column <  img.width; column ++) //right side search
+	 {
+            if (img.data[row * img.width + column] == white_pixel) {
+                
+                ball_pixel_num++ ;
+		if (ball_pixel_num > 80){// if there is greater than 80 pixels in white
+        		drive_robot(0.0, 0.1); 
+			ROS_INFO_STREAM("Turn to Right");
+			break;					
+		} 
+    	    }
+	 }
+
+
+
+
     }
-    if (count_total == 0) {
+    if (ball_pixel_num == 0) {
         x = 0.0;
         z = 0.0;
 	ROS_INFO_STREAM("No ball in front, stop");
+	drive_robot(x, z);
     }
-    else {
-	ROS_INFO_STREAM("There is a ball in front, go go!");
-        x = 0.1;
-	z = 0.5 * (img.width / 2.0 - idx_center_ball);
 
-        // Calculate the average offset (from -step/2.0 to +step/2.0)
-        // Normalize the average offset (from -1.0 to 1.0)
-        // Multiply with magic number -4.0 to turn
-        //z = -4.0 * offset_accumulated / count_total / (img.width /2.0);
-    }
-    
-    // Send request to service
-    drive_robot(x, z);
-
+  
 }
-***/
 
+/***
 void process_image_callback(const sensor_msgs::Image img)
 {
     int white_pixel = 255;
@@ -126,7 +149,7 @@ void process_image_callback(const sensor_msgs::Image img)
 	ROS_INFO_STREAM("Turn to Left");
     }
 }
-
+***/
 
 
 int main(int argc, char** argv)
